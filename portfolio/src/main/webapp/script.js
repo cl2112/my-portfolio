7962698040a3bf87a95e567949aab66b1bba43e4
Christian Liguori <23705622+cl2112@users.noreply.github.com>
2021-06-03 05:26:05 +0000

    Change the display random greeting feature to display random quote.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,17 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+/** 
+ * Adds a random quote from the TV show Parks and Rec to the page.
+*/
+const addRandomQuote = () => {
+  const quotes = [
+      "I have no idea what I'm doing, but I know I'm doing it really, " + 
+        "really well",
+      "I typed your symptoms into the thing up here and it says you could " + 
+        "have network connectivity problems.",
+      "Ron: I'll have the number 8. Waiter: That's a party platter. It " + 
+        "serves 12 people. Ron: I know what I'm about, son.",
+      "I'm allergic to sushi. Everytime I eat more than 80 pieces I throw up.",
+  ];
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+    // Pick a random quote.
+    const quote = quotes[Math.floor(Math.random() * quotes.length)];
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+    // Add it to the page.
+    const quoteContainer = document.getElementById("quote-container");
+    quoteContainer.innerText = quote;
 }
