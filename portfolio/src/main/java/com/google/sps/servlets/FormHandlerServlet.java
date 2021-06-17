cb0fa32ed4c690bc700d2b3a0b1e7d97062bb1e1
Christian Liguori <23705622+cl2112@users.noreply.github.com>
2021-06-17 22:50:07 +0000

    Change form route from form-handler to posts.

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/form-handler")
+@WebServlet("/posts")
 public class FormHandlerServlet extends HttpServlet {
 
   @Override
